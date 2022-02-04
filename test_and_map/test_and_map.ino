33618b489bc24fd03a137e843b02d776dbc574b0
j3qq4hch <j3qq4hch@gmail.com>
2022-02-04 17:02:37 +0300

    Update test_and_map.ino

--- test_and_map/test_and_map.ino
+++ test_and_map/test_and_map.ino
@@ -10,53 +10,71 @@
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
 CRGB leds[NUM_LEDS];
-CRGB colors[] = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::White, CRGB::Black };
+CRGB colors[] = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::White};
 
 
 int esagono_get_led_index(u8 strip, u8 pos);
 void strip_turn_off(u8 strip_num, u16 delay_ms);
 void strip_turn_on(u8 strip_num, CRGB color, u16 delay_ms);
 void esagono_node_blow(esagono_node_t * node, CRGB color, u16 delay_ms);
-void setup() 
-{ 
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  
+void setup()
+{
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
   //FastLED.setBrightness(16);
-  Serial.begin(115200); 
-  for(int i = 0; i < NUM_LEDS; i++)
+  Serial.begin(115200);
+  for (int i = 0; i < NUM_LEDS; i++)
   {
     leds[i] = CRGB::Black;
   }
   FastLED.show();
-
   Serial.write("\r\nEsagono test and setup sketch.");
   Serial.write("\r\nPress + to light up the next LED strip, press - to turn it off.");
+  Serial.write("\r\nInput node number ro test its mapping.");
 
-
-
-  for(u8 i = 0; i < ARRAY_SIZE(nodes);i++)
+  u8 color_num = 0;
+  
+  while (1)
   {
-    esagono_node_blow(&nodes[i], CRGB::Green, 0);
-    esagono_node_blow(&nodes[i], CRGB::Black, 0);
+    for (u8 i = 0; i < ARRAY_SIZE(nodes); i++)
+    {
+      esagono_node_blow(&nodes[i], colors[color_num], 20);
+      esagono_node_blow(&nodes[i], CRGB::Black, 20);
+    }
+    color_num = (color_num + 1) % ARRAY_SIZE(colors);
   }
-  Serial.write("\r\nNodes testing also available.");
 }
 
 
+void esagono_node_blow(esagono_node_t * node, CRGB color, u16 delay_ms)
+{
+  for (u8 dist = 0; dist < LEDS_PER_STRIP; dist++)
+  {
+    for (u8 port = 0; port < 6; port++)
+    {
+      if (node->dir[port])
+      {
+        leds[distance_from_node_to_led_index(node, port, dist)] = color;
+      }
+    }
+     FastLED.show();
+     delay(delay_ms);
+  }
+}
 
 int index = -1;
 u8 c = 0;
 
-void loop() 
+void loop()
 {
-   if (Serial.available() > 0) 
+  if (Serial.available() > 0)
   {
-      c = Serial.read();
-      switch(c)
-      {
-        case '+':
+    c = Serial.read();
+    switch (c)
+    {
+      case '+':
         {
-          if(index < STRIPS_NUM - 1)
-          {  
+          if (index < STRIPS_NUM - 1)
+          {
             index++;
             Serial.print("\r\nTurning ON strip ");
             Serial.print(index);
@@ -64,10 +82,10 @@ void loop()
           }
           break;
         }
-        case '-':
+      case '-':
         {
-          if(index > -1)
-          {  
+          if (index > -1)
+          {
             Serial.print("\r\nTurning OFF strip ");
             Serial.print(index);
             strip_turn_off(index, 20);
@@ -75,26 +93,27 @@ void loop()
           }
           break;
         }
-      }
+    }
   }
 }
 
-int esagono_get_led_index(u8 strip, u8 pos)
+inline int esagono_get_led_index(u8 strip, u8 pos)
 {
-  if (pos > 4) pos = 9 - (pos - 5);
-  return (strip * 10 + pos); 
+  //if (pos > 4) pos = 9 - (pos - 5);
+  if (pos > 4) pos = 14 - pos;
+  return (strip * 10 + pos);
 }
 
 int distance_from_node_to_led_index(esagono_node_t * node, u8 port, u8 distance)
 {
-  if(node->dir[port] == 0) return -1;
-  if(node->dir[port] == 1) return esagono_get_led_index(node->id[port], distance);
-  if(node->dir[port] == 2) return esagono_get_led_index(node->id[port], 9 - distance);
+  if (node->dir[port] == 0) return -1;
+  if (node->dir[port] == 1) return esagono_get_led_index(node->id[port], distance);
+  if (node->dir[port] == 2) return esagono_get_led_index(node->id[port], 9 - distance);
 }
 
 void strip_turn_on(u8 strip_num, CRGB color, u16 delay_ms)
 {
-  for(u8 i = 0; i < LEDS_PER_STRIP; i++)
+  for (u8 i = 0; i < LEDS_PER_STRIP; i++)
   {
     leds[esagono_get_led_index(strip_num, i)] = color;
     FastLED.show();
@@ -105,34 +124,10 @@ void strip_turn_on(u8 strip_num, CRGB color, u16 delay_ms)
 
 void strip_turn_off(u8 strip_num, u16 delay_ms)
 {
-  for(int i = LEDS_PER_STRIP - 1; i >= 0; i--)
+  for (int i = LEDS_PER_STRIP - 1; i >= 0; i--)
   {
     leds[esagono_get_led_index(strip_num, i)] = CRGB::Black;
     FastLED.show();
     delay(delay_ms);
-  }
-}
-
-void esagono_node_blow(esagono_node_t * node, CRGB color, u16 delay_ms)
-{
-  for(u8 dist = 0; dist < LEDS_PER_STRIP; dist++)
-  {
- //  Serial.print("\r\nDistance from the node: ");
- //   Serial.print(dist);
-      
-    for(u8 port = 0; port < 6; port++)
-    {
-    if(node->dir[port])
-    {
- //    int index = distance_from_node_to_led_index(node, port, dist);
- //    Serial.print("\r\nFound strip on port ");
- //    Serial.print(port);
- //    Serial.print("\r\nLed index is ");
- //    Serial.print(index);
-      leds[distance_from_node_to_led_index(node, port, dist)] = color;
-    }
-    FastLED.show();
-    delay(delay_ms);
-    }
   }
 }
